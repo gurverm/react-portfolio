5b7858e936aaeda155407373e2c85b86b3baba7d
Gurveer Madurai <maduraigurveer@gmail.com>
2023-07-24 13:32:10 -0700

    header file structure and logic updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import "./App.css";
+import Header from './components/header/Header';
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
@@ -9,18 +12,9 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+    <Header/>
+    </>
   );
 };
 
