01d201d7f72a79eea1311ff2b4a34c33c3564468
Gurveer Madurai <maduraigurveer@gmail.com>
2023-07-26 10:56:43 -0700

    footer debug

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -6,7 +6,7 @@ import '../../footer/footer.css'
 
 const About = () => {
   return (
-    <section className='about section more-content-page' id='about'>
+    <section className='about section' id='about'>
         <h2 className='section_title'>About Me</h2>
         <span className='section_subtitle'></span>
 
